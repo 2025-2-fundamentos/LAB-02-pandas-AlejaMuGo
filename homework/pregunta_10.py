a0129a8d068da93e852aa05977a9b618b7b6bde2
Aleja Muñoz <mamunozgo@unal.edu.co>
2025-11-17 16:44:04 -0500

    Solucion preguntas taller pandas

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_10():
     """
@@ -20,3 +20,9 @@ def pregunta_10():
     D                   1:2:3:5:5:7
     E   1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    df = df.sort_values(["c1", "c2"])
+    serie = df.groupby("c1")["c2"].apply(lambda x: ":".join(str(v) for v in x))
+    serie.index.name = "_c1"
+    return serie.to_frame(name="c2")
+print(pregunta_10())
